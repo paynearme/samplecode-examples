c0e9498a4815c600d4e4b6018e21dcaf46bb5527
Matt Scott <matt.scott@paynearme.com>
2014-08-15 11:08:38 -0700

    APPS-5201 Added syntax matching fix to spec helper

--- ruby/spec/spec_helper.rb
+++ ruby/spec/spec_helper.rb
@@ -2,6 +2,14 @@ require 'factory_girl'
 require 'simplecov'
 SimpleCov.start
 
+RSpec.configure do |config|
+  config.expect_with :rspec do |c|
+    c.syntax = [:should, :expect]
+  end
+  config.mock_with :rspec do |c|
+    c.syntax = [:should, :expect]
+  end
+end
 
 class FakeResponse
   attr_accessor :body
